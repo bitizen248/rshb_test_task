9ad97e1147f79928b275af724b5cdaea654dccf4
Ivan Dmitirev <idmitriev@black-sun.ru>
2020-09-09 22:53:04 +0300

    Сгенерованные json парсеры

---
+++ lib/models/farmer.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'farmer.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Farmer _$FarmerFromJson(Map<String, dynamic> json) {
+  return Farmer(
+    json['id'] as int,
+    json['title'] as String,
+  );
+}
+
+Map<String, dynamic> _$FarmerToJson(Farmer instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+    };
