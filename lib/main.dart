fdcdee0059cdf5ec08f4231ae2b51221e819214b
Ivan Dmitirev <idmitriev@black-sun.ru>
2020-09-09 22:54:38 +0300

    Всякие мелочи

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:divan_test_task_rshb/app.dart';
+import 'package:divan_test_task_rshb/models/sort_order.dart';
+import 'package:divan_test_task_rshb/resources/products_api.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(RshbApp());
